0bfb91fa82322b1fa57c6a3102122497189f6603
juliusz jakubowski <juliusz.jakubowski@noreply.github.com>
2024-01-14 13:55:45 +0100

    start stop Counter.tsx

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './Application.scss';
 import Counter from '@components/Counter/Counter';
 import AddCounter from '@components/AddCounter/AddCounter';
 
 const Application: React.FC = () => {
-
   useEffect(() => {
     console.log('App did mount');
   }, []);
 
   return (
     <main>
-
+      <Counter/>
+      <Counter/>
       <Counter/>
       <AddCounter/>
     </main>
--- src/renderer/components/Counter/Counter.tsx
+++ src/renderer/components/Counter/Counter.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+
+import formatTime from '@renderer/utils/formatTime';
 
 import './Counter.scss';
 
 const Counter = () => {
+  const [playPause, setPlayPause] = useState(false)
+  const [timerValue, setTimerValue] = useState<number>(0)
+  const [intervalID, setIntervalID] = useState<NodeJS.Timeout | number | null>(null)
+
+  const runInterval = () => {
+    const interval = setInterval(() => {
+      setTimerValue(prev => prev + 1)
+    }, 1000);
+
+    setIntervalID(interval)
+  }
+
+  const pauseInterval = () => clearInterval(intervalID);
+
+  const playPauseToggle = () => setPlayPause(prev => !prev)
+
+
+  useLayoutEffect(() => {
+    return () => {
+      clearInterval(intervalID);
+    }
+  }, [])
+
+  useEffect(() => {
+    if(playPause) {
+      runInterval()
+    }
+    else {
+      pauseInterval()
+    }
+  }, [playPause]);
+
   return (
     <div className='counter'>
-    <h1>test</h1>
+      <div className='title'>title</div>
+      <span className='management'>
+        <span>{formatTime(timerValue)}</span>
+        <span className='playPause' onClick={playPauseToggle}>&#x23EF;</span>
+      </span>
     </div>
   )
 }
